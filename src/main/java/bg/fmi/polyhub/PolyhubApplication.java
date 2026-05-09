0aa9380c832285fbb1a058def36dcf3b97462488
Teodor Dichev <teodichev@gmail.com>
2026-05-09 18:38:58 +0300

    Added maven project files and updated properties to enable db connections.

---
+++ src/main/java/bg/fmi/polyhub/PolyhubApplication.java
@@ -0,0 +1,13 @@
+package bg.fmi.polyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyhubApplication.class, args);
+	}
+
+}
