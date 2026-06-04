e203e071e5f826659e78bb3e0abeb6b616692013
Teodor Dichev <teodichev@gmail.com>
2026-06-04 23:58:35 +0300

    Added PrePersist on createdAt attributes

--- src/main/java/bg/fmi/polyhub/entities/Party.java
+++ src/main/java/bg/fmi/polyhub/entities/Party.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -65,4 +66,10 @@ public class Party {
 
     @Column(name = "spec_social_axis")
     private Double specSocialAxis;
+
+    // because hibernate changes it to null??? and we insert null...
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
--- src/main/java/bg/fmi/polyhub/entities/User.java
+++ src/main/java/bg/fmi/polyhub/entities/User.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -46,4 +47,10 @@ public class User {
     @ManyToOne(optional = false)
     @JoinColumn(name = "role_id", nullable = false)
     private UserRole role;
+
+    // because hibernate changes it to null??? and we insert null...
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
